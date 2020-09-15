e8095bd258fc8503330f33a877c20636f75b9d1e
Ashish Chahal <ashish.chahal27@gmail.com>
2020-09-15 11:44:22 +0530

    get data from Spotify to show playlists

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,8 +4,12 @@ import SidebarOption from "./SidebarOption";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { useDataLayerValue } from "../DataLayer";
 
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+
+  console.log(playlists, "playlists 🎵👧");
   return (
     <div className="sidebar">
       <img
@@ -21,9 +25,12 @@ function Sidebar() {
       <strong className="sidebar__title">PLAYLISTS</strong>
       <hr />
 
-      <SidebarOption title="Hip hop" />
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption title={playlist.name} />
+      ))}
+      {/* <SidebarOption title="Hip hop" />
       <SidebarOption title="Rock" />
-      <SidebarOption title="RnB" />
+      <SidebarOption title="RnB" /> */}
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -4,13 +4,13 @@ export const initialState = {
   playing: false,
   item: null,
   // REMOVE after finished debugging
-  token:
-    "BQDrfci7LFPBFd5VZ2ZvoacUNXpeU5CW8C8kItICHxWqCxFM9NBARmxVyLR3WSCs2FU4kLAVtxREpPRJ4REXrOn9oJ5S61hldRp7g1KVJnXx5b6ECyGOi-UPrPHRugbef9dxTru370oWQVefYUOroUCrvV56A6jcl-hAb7OdcMuyuQEIrhL8XTjy",
+  //   token:
+  //     "BQDrfci7LFPBFd5VZ2ZvoacUNXpeU5CW8C8kItICHxWqCxFM9NBARmxVyLR3WSCs2FU4kLAVtxREpPRJ4REXrOn9oJ5S61hldRp7g1KVJnXx5b6ECyGOi-UPrPHRugbef9dxTru370oWQVefYUOroUCrvV56A6jcl-hAb7OdcMuyuQEIrhL8XTjy",
 };
 
 const reducer = (state, action) => {
   console.log("action in reducer", action);
-
+  // we listens for action inside the reducer
   // Action --> type, [payload]
   switch (action.type) {
     case "SET_USER":
@@ -22,6 +22,11 @@ const reducer = (state, action) => {
       return {
         ...state,
         token: action.token,
+      };
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
       };
     default:
       return state;
