24e6aa58ca82ddd64825e162a21884ab3970aeff
Ashish Chahal <ashish.chahal27@gmail.com>
2020-09-15 15:25:19 +0530

    header avatar & username show dynamically done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,11 @@ import React from "react";
 import "../Styles/Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "../DataLayer";
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
+  console.log("👨‍💻👨‍💻👨‍💻👨‍💻👨‍💻👨‍💻", user);
   return (
     <div className="header">
       <div className="header__left">
@@ -14,8 +17,8 @@ function Header() {
         />
       </div>
       <div className="header__right">
-        <Avatar src="" alt="PA" />
-        <h4>Ashish Chahal</h4>
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );
